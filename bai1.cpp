57b88cad8fa288094935923284b9b7228f9a2aa5
nguyenthiphuong28 <162585178+nguyenthiphuong28@users.noreply.github.com>
2024-05-10 19:33:08 +0700

    Add files via upload

--- bai1.cpp
+++ bai1.cpp
@@ -1,26 +1,57 @@
 #include<stdio.h>
 #include<string.h>
+
+char nhap[100];
+
 void Nhapchuoi(){
-	char nhap[100];
 	printf("Nhap chuoi ky tu: ");
-	scanf("%s",nhap);
-	
+	gets(nhap);
 }
-int dem_NA(char s[]){
-	char NA[6] = "aeiou";
-	int dem = 0, i = 0;
-	while (s[i]!='\0')
-	{
-		for(int j= 0; j < strlen(NA); j++)
-		    if(s[i] == NA[j] || s[i] + 32 == NA[j]) dem++;
+
+void dem_NA(char s[]){
+	int i=0;
+	int na[5]={0,0,0,0,0};//a/A e/E i/I o/O u/U
+	
+	while(s[i]!='\0'){
+		switch(s[i]){
+			case 'a':
+			case 'A':{
+				na[0]++;				
+				break;
+			}
+			case 'e':
+			case 'E':{
+				na[1]++;				
+				break;
+			}
+			case 'i':
+			case 'I':{
+				na[2]++;				
+				break;
+			}
+			case 'o':
+			case 'O':{
+				na[3]++;				
+				break;
+			}
+			case 'u':
+			case 'U':{
+				na[4]++;				
+				break;
+			}
+		}
 		i++;
-    }
-    return dem;
+	}
+	//in mang
+	printf("\na/A\te/E\ti/I\to/O\tu/U\n");
+	for(int i=0; i<5; i++){
+		printf("%d\t",na[i]);
+	}
 }
 int main(){
-	char nhap[100];
-    printf("Nhap chuoi ky tu: ");
-    scanf("%s", nhap);
-    printf("Nhap chuoi vua nhap:%s", nhap);
-    printf("in so luong NA:%s\n");
+	Nhapchuoi();
+    printf("Nhap chuoi vua nhap: ");
+    puts(nhap);
+    printf("\nSo nguyen am trong chuoi: ");
+    dem_NA(nhap);
 }
