e87170655bfae24cd3d72948211aa33b5db0fe92
nguyenthiphuong28 <162585178+nguyenthiphuong28@users.noreply.github.com>
2024-05-10 18:46:46 +0700

    Add files via upload

--- bai1.cpp
+++ bai1.cpp
@@ -1,62 +1,26 @@
 #include<stdio.h>
-
-void nhapDS(int a[], int &n)
-{
-	do
-	{
-		printf("Nhap so luong phan tu: ");
-		scanf("%d", &n);
-	} while(n <= 5 || n >= 10);
+#include<string.h>
+void Nhapchuoi(){
+	char nhap[100];
+	printf("Nhap chuoi ky tu: ");
+	scanf("%s",nhap);
 	
-	for(int i = 0; i < n; i++)
+}
+int dem_NA(char s[]){
+	char NA[6] = "aeiou";
+	int dem = 0, i = 0;
+	while (s[i]!='\0')
 	{
-		printf("a[%d]=", i);
-		scanf("%d", &a[i]);
-	}
-}
-void xuatDS(int a[], int n)
-{
-	printf("Mang vua nhap: ");
-	for(int i = 0; i < n; i++)
-		printf("%d ", a[i]);
-}
-int snt(int a){
-	if (a<2) 
-	   return 0;
-	else
-	for (int i=2;i<a;i++){
-		if (a%i==0){
-		  return 0;
-	}		  
-	}
-	return 1;
-}
-int shh(int a)
-{
-	int s=0;
-	for(int i = 0; i<a; i++){
-		if(a%i == 0)
-		s += 1;
-		
-	}
-	if(s == a)
-	   return 0;
-	else
-	   return 1;
-	
+		for(int j= 0; j < strlen(NA); j++)
+		    if(s[i] == NA[j] || s[i] + 32 == NA[j]) dem++;
+		i++;
+    }
+    return dem;
 }
 int main(){
-	int a[10];
-	int n;
-	nhapDS(a,n);
-	xuatDS(a,n);
-	printf("nso nguyen to cua day\n");
-	for (int i=0;i<n;i++){
-		if (snt(a[i]))
-		  printf("%d\t",a[i]);
-	}
-	printf("\nso hoan hao cua day\n");
-	for (int i=0;i<n;i++)
-		if (shh(a[i]))
-		  printf("%d\t",a[i]);
+	char nhap[100];
+    printf("Nhap chuoi ky tu: ");
+    scanf("%s", nhap);
+    printf("Nhap chuoi vua nhap:%s", nhap);
+    printf("in so luong NA:%s\n");
 }
